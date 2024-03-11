cf36a37c427f97269931f56b9fabe19a0b15bc00
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-03-11 19:03:40 +0000

    gemma requires some authentication before use

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, make_response, request
+from modules.Speaker import Speaker
 
 
 from modules.Weather import Open_Metro, Visual_Crossing
@@ -21,6 +22,5 @@ def user_message():
 
 
 if __name__ == "__main__":
-    vc = Visual_Crossing()
-    om = Open_Metro()
-    om.format_report()
+    speaker = Speaker()
+    speaker.talk()
--- webserver/modules/Speaker.py
+++ webserver/modules/Speaker.py
@@ -1,19 +1,15 @@
-import torch
-from transformers import AutoTokenizer, TFAutoModelForCausalLM
-
-# from hugging face docs
+from transformers import GemmaModel
 
 
 class Speaker:
     def __init__(self):
-        self.model = "gpt2"
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
-        self.model_instance = TFAutoModelForCausalLM.from_pretrained(
-            self.model)
-        self.model_instance.config.pad_token_id = self.model_instance.config.eos_token_id
+        self.model = GemmaModel.from_pretrained("google/gemma-2b")
 
-    def talk(self, input):
-        model_inputs = self.tokenizer([input], return_tensors="tf")
-        model_generate = self.model_instance.generate(
-            **model_inputs, do_sample=True, seed=((42, 0)))
-        print("out: ", self.tokenizer.decode(model_generate[0]))
+    def talk(self):
+        while True:
+            user_input = input(">>")
+            if user_input == "end":
+                break
+            else:
+                response = self.model.generate(user_input)
+                print(response)
