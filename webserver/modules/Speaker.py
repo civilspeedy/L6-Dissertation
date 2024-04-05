cfaeb703b63cb284cf8b2fb33975b5c0f6f1b9ad
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-04-05 18:24:32 +0100

    I'm not sure what to do

--- webserver/modules/Speaker.py
+++ webserver/modules/Speaker.py
@@ -9,8 +9,17 @@ class Speaker:
         self.model = AutoModelForCausalLM.from_pretrained(
             "google/gemma-2b", device_map="auto"
         )
-        input_text = "Write me a poem about Machine Learning."
+
+        self.model.config.hidden_activation = "gelu_pytorch_tanh"
+
+        input_text = "hello"
         input_ids = self.tokenizer(input_text, return_tensors="pt")
         input_ids = input_ids.to(self.model.device)
-        outputs = self.model.generate(**input_ids)
+        outputs = self.model.generate(
+            **input_ids,
+            max_new_tokens=200,
+            early_stopping=True,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
+
         print(self.tokenizer.decode(outputs[0]))
